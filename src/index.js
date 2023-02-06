2f22f51f7c68eacb2e0fa55105d717438687de9d
Ahmed <ahmedsalem.official2@gmail.com>
2023-02-06 12:05:42 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -29,11 +29,11 @@ i18n
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  //<React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  // </React.StrictMode>
+  //</React.StrictMode>
 );
 
 
