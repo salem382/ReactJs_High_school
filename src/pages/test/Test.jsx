a610c235061be16f03b9c56940b1afef8bc39b2a
Ahmed <ahmedsalem.official2@gmail.com>
2023-02-08 08:33:47 +0200

    cached som data

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import LoginNavbar from "../../components/general/lgoinNavbar/LoginNavbar";
@@ -7,8 +7,6 @@ import Wrapper from "../../components/general/Wrapper/Wrapper";
 import Quiz from "../../components/quiz/Quiz";
 import {getUQuizess} from '../../store/quizSlice';
 import PageReload from "../../components/general/pageReload/PageReload";
-import ShowResult from "../../components/general/showResult/ShowResult";
-import { useLocation } from "react-router-dom";
 import {setIsEnd, defaultTotalSubmited} from '../../store/showResultSlice';
 import LessonsTopPart from "../../components/lessonsComponent/lessonsTopPart/LessonsTopPart";
 
@@ -20,7 +18,6 @@ const Test = () => {
     const dispatch = useDispatch();
 
     const params = useParams();
-    const location = useLocation();
    
     const {isLoading} = useSelector(state => state.quiz);
 
